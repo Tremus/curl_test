e56f0ab6ce0a8bd1a52671e89a942b1617f04781
Trémus <19359682+Tremus@users.noreply.github.com>
2023-05-23 12:51:26 +1000

    Added POST JSON method

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+static struct curl_slist POST_HEADERS[] = {
+    {.data = "Content-Type: applicaion/json", .next = NULL}};
+
 struct https_response
 {
     char* body;
@@ -10,8 +13,8 @@ struct https_response
     long status;
 };
 
-static size_t https_request_cb(void* data, size_t size, size_t nmemb,
-                               void* clientp)
+static size_t
+https_request_cb(void* data, size_t size, size_t nmemb, void* clientp)
 {
     size_t realsize            = size * nmemb;
     struct https_response* mem = (struct https_response*)clientp;
@@ -29,8 +32,8 @@ static size_t https_request_cb(void* data, size_t size, size_t nmemb,
 };
 
 // Use this for simple GET requests
-static CURLcode https_get(CURL* session, struct https_response* res,
-                          const char* url)
+static CURLcode
+https_get(CURL* session, struct https_response* res, const char* url)
 {
     /* send all data to this function  */
     curl_easy_setopt(session, CURLOPT_WRITEFUNCTION, https_request_cb);
@@ -47,19 +50,45 @@ static CURLcode https_get(CURL* session, struct https_response* res,
     return code;
 }
 
+// Use this for simple JSON POST requests
+static CURLcode https_post_json(
+    CURL* session,
+    struct https_response* res,
+    const char* url,
+    const char* postdata)
+{
+    /* send all data to this function  */
+    curl_easy_setopt(session, CURLOPT_WRITEFUNCTION, https_request_cb);
+
+    /* we pass our 'https_response' struct to the callback function */
+    curl_easy_setopt(session, CURLOPT_WRITEDATA, (void*)res);
+
+    curl_easy_setopt(session, CURLOPT_URL, url);
+    curl_easy_setopt(session, CURLOPT_COPYPOSTFIELDS, postdata);
+    curl_easy_setopt(session, CURLOPT_HTTPHEADER, POST_HEADERS);
+
+    /* send a request */
+    CURLcode code = curl_easy_perform(session);
+
+    curl_easy_getinfo(session, CURLINFO_RESPONSE_CODE, &res->status);
+    return code;
+}
+
 // Useful examples:
 // https://curl.se/libcurl/c/https.html
 // https://curl.se/libcurl/c/CURLOPT_WRITEFUNCTION.html
 int main()
 {
     CURLcode code;
-    struct https_response res = {0, 0, 0};
-    CURL* session             = curl_easy_init();
+    struct https_response res;
+    CURL* session = curl_easy_init();
 
     curl_global_init(CURL_GLOBAL_DEFAULT);
 
+    // Test GET
     if (session)
     {
+        memset(&res, 0, sizeof(res));
         code = https_get(session, &res, "https://example.com/");
 
         if (code == CURLE_OK)
@@ -70,8 +99,40 @@ int main()
         }
         else
         {
-            fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                    curl_easy_strerror(code));
+            fprintf(
+                stderr,
+                "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(code));
+        }
+
+        /* remember to free the buffer */
+        free(res.body);
+        curl_easy_cleanup(session);
+    }
+
+    // Test POST
+    session = curl_easy_init();
+    if (session)
+    {
+        memset(&res, 0, sizeof(res));
+        code = https_post_json(
+            session,
+            &res,
+            "https://httpbin.org/post",
+            "{\"greeting\": \"Bonjour\"}");
+
+        if (code == CURLE_OK)
+        {
+            printf("HTTP Status: %ld\n", res.status);
+            printf("Response length: %zu\n", res.size);
+            printf("Response: %s\n", res.body);
+        }
+        else
+        {
+            fprintf(
+                stderr,
+                "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(code));
         }
 
         /* remember to free the buffer */
